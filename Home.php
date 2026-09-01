e4cfa4d1dd92123801033a3418982b97dcff57ea
IryawanLuvYuna <iryawanfikriansyah66@gmail.com>
2026-09-01 19:35:10 +0700

    update tampilan footer

--- Home.php
+++ Home.php
@@ -9,8 +9,7 @@ include 'header.php';
         <p>
           Badan Pusat Statistik (BPS) Provinsi Sulawesi Tengah menyediakan data
           statistik yang akurat, terpercaya, dan terkini untuk mendukung
-          pembangunan daerah dan pengambilan keputusan berbasis data dan mencapai
-          Indonesia cemas 2067.
+          pembangunan daerah dan pengambilan keputusan berbasis data.
         </p>
       </div>
       <div class="hero-right">
@@ -18,21 +17,6 @@ include 'header.php';
           <img src="aset/maskot_sensus.png" alt="Maskot Sensus">
         </div>
       </div>
-    </div>
-
-    <div class="hero-bottom">
-      <div class="about-strip-text">
-        <h3>Tentang BPS Provinsi Sulawesi Tengah</h3>
-        <p>
-          BPS adalah lembaga pemerintah non-kementerian yang bertugas
-          menyelenggarakan kegiatan statistik sesuai peraturan perundang-undangan.
-          Data kami menjadi acuan perencanaan dan evaluasi pembangunan di Sulawesi Tengah.
-        </p>
-      </div>
-      <div class="about-strip-visual">
-        <img src="aset/logo-footer.png" alt="Logo BerAKHLAK">
-      </div>
-      <a href="https://sulteng.bps.go.id" target="_blank">Kunjungi Website Resmi</a>
     </div>
   </section>
 
--- footer.php
+++ footer.php
@@ -1,8 +1,37 @@
-<footer>
-  <address>
+</main>
+
+<footer class="site-footer">
+  <div class="footer-main">
+    <div class="footer-col footer-about">
+      <h3>Tentang BPS Provinsi Sulawesi Tengah</h3>
+      <p>
+        BPS adalah lembaga pemerintah non-kementerian yang bertugas
+        menyelenggarakan kegiatan statistik sesuai peraturan perundang-undangan.
+        Data kami menjadi acuan perencanaan dan evaluasi pembangunan di Sulawesi Tengah.
+      </p>
+
+      <div class="footer-logo">
+        <img src="aset/logo-footer.png" alt="Logo BerAKHLAK">
+      </div>
+
+      <div class="footer-contact">
+        <h4>Informasi Kontak</h4>
+        <p>Jl. Prof. Mohammad Yamin No. 48, Palu, Sulawesi Tengah 94114</p>
+        <p>Telepon: (0451) 483610</p>
+        <p>Email: <a href="mailto:bps7200@bps.go.id">bps7200@bps.go.id</a></p>
+      </div>
+    </div>
+
+    <div class="footer-col footer-cta">
+      <a href="https://sulteng.bps.go.id" target="_blank" class="btn-official">Kunjungi Website Resmi</a>
+    </div>
+  </div>
+
+  <div class="footer-credit">
     Created by Moh. Iryawan F.
     <a href="mailto:222413656@stis.ac.id">(222413656@stis.ac.id)</a>
-  </address>
+  </div>
 </footer>
-</body>
+
+  </body>
 </html>
